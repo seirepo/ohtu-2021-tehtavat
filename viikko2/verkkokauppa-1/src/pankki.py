b0f79fc40387f55c8e119c8fd957a360c83a0674
susanna <susannae.repo@gmail.com>
2021-11-10 15:45:56 +0200

    exercise 12 edit pankki

--- viikko2/verkkokauppa-1/src/pankki.py
+++ viikko2/verkkokauppa-1/src/pankki.py
@@ -1,9 +1,9 @@
-from kirjanpito import Kirjanpito
+from kirjanpito import kirjanpito as default_kirjanpito
 
 
 class Pankki:
 
-    def __init__(self, kirjanpito):
+    def __init__(self, kirjanpito=default_kirjanpito):
         self._kirjanpito = kirjanpito
         pass
 
@@ -14,3 +14,5 @@ class Pankki:
 
         # täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
         return True
+
+pankki = Pankki()
