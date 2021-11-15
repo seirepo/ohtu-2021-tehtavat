18860e6b5d876907f38c1d24483c98816c3b2fef
susanna <susannae.repo@gmail.com>
2021-11-15 15:06:49 +0200

    edit main

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,29 +1,14 @@
 import requests
-from player import Player
+from player_reader import PlayerReader
+from player_stats import PlayerStats
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    print("JSON-muotoinen vastaus:")
-    print(type(response[0]))
-
-    players = []
-    nationalities = set()
-
-    for player_dict in response:
-        player = Player(
-            player_dict['name'], player_dict['nationality'],
-            player_dict['team'], player_dict['games'],
-            player_dict['assists'], player_dict['goals']
-        )
-        nationalities.add(player_dict['nationality'])
-        players.append(player)
-
-    print("Oliot:")
-
-    players_fin = filter(lambda x: x.nationality == "FIN", players)
-    for player in sorted(list(players_fin), key=lambda x: x.goals + x.assists, reverse=True):
+    for player in players:
         print(player)
 
 if __name__ == "__main__":
