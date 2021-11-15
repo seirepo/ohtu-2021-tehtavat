65c6f447cd214564085443be7562f1522b0ae132
susanna <susannae.repo@gmail.com>
2021-11-15 14:23:28 +0200

    edit player constructor, print only finnish players

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,38 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(type(response[0]))
+
+    players = []
+    nationalities = set()
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'], player_dict['nationality'],
+            player_dict['team'], player_dict['games'],
+            player_dict['assists'], player_dict['goals']
+        )
+        nationalities.add(player_dict['nationality'])
+        players.append(player)
+
+    print("Oliot:")
+
+    #for player in players:
+    #    print(player)
+
+    for nat in nationalities:
+        if (nat == "FIN"):
+            print("players from " + nat)
+            for player in players:
+                if player.nationality == nat:
+                    print(player)
+
+
 
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, team,
+                 games, assists, goals):
         self.name = name
+        self.nationality = nationality
+        self.team = team
+        self.games = games
+        self.assists = assists
+        self.goals = goals
     
     def __str__(self):
-        return self.name
+        return "\t" + self.name + " team " + self.team + " goals " + str(self.goals) + " assists " + str(self.assists)
