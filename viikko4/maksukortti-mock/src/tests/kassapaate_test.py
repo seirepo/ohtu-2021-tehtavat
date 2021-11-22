9186d61e8023bc9db5de25a2bdb9a893ec32c7d9
susanna <susannae.repo@gmail.com>
2021-11-22 11:37:39 +0200

    2. Yksikkötestaus ja riippuvuudet: mock-kirjasto, osa 2: testit ok

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,15 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_voi_ladata_rahaa_kassapaatteella(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, 5)
+        maksukortti_mock.lataa.assert_called_with(5)
+
+    def test_kassapaate_ei_lataa_kortille_negatiivista_summaa(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, -10)
+        maksukortti_mock.lataa.assert_not_called()
