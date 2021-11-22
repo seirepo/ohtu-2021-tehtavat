864344cafba74145a1066d0fe1ec9fad9302fbba
susanna <susannae.repo@gmail.com>
2021-11-22 14:59:33 +0200

    3. Yksikkötestaus ja riippuvuudet: mock-kirjasto, osa 3 valmis

--- viikko4/verkkokauppa-1/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa-1/src/tests/kauppa_test.py
@@ -33,13 +33,14 @@ class TestKauppa(unittest.TestCase):
 
         self.kauppa = Kauppa(self.varasto_mock, self.pankki_mock, self.viitegeneraattori_mock)
 
-    def test_ostoksen_paaytyttya_pankin_metodia_tilisiirto_kutsutaan(self):
-        
         # palautetaan aina arvo 42
         self.viitegeneraattori_mock.uusi.return_value = 42
 
         # tehdään ostokset
         self.kauppa.aloita_asiointi()
+
+    def test_ostoksen_paaytyttya_pankin_metodia_tilisiirto_kutsutaan(self):
+
         self.kauppa.lisaa_koriin(1)
         self.kauppa.tilimaksu("pekka", "12345")
 
@@ -48,18 +49,14 @@ class TestKauppa(unittest.TestCase):
         # toistaiseksi ei välitetä kutsuun liittyvistä argumenteista
 
     def test_ostoksen_paaytyttya_pankin_metodia_tilisiirto_kutsutaan_oikeilla_parametreilla(self):
-        self.viitegeneraattori_mock.uusi.return_value = 42
 
-        self.kauppa.aloita_asiointi()
         self.kauppa.lisaa_koriin(1)
         self.kauppa.tilimaksu("pekka", "12345")
 
         self.pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", ANY, 5)
 
     def test_tilisiirtoa_kutsutaan_oikein_kun_ostetaan_kaksi_eri_varastossa_olevaa_tuotetta(self):
-        self.viitegeneraattori_mock.uusi.return_value = 42
 
-        self.kauppa.aloita_asiointi()
         self.kauppa.lisaa_koriin(1)
         self.kauppa.lisaa_koriin(2)
         self.kauppa.tilimaksu("maria", "11233")
@@ -67,9 +64,7 @@ class TestKauppa(unittest.TestCase):
         self.pankki_mock.tilisiirto.assert_called_with("maria", 42, "11233", ANY, 8)
 
     def test_tilisiirtoa_kutsutaan_oikein_kun_ostetaan_kaksi_samaa_varastossa_olevaa_tuotetta(self):
-        self.viitegeneraattori_mock.uusi.return_value = 42
 
-        self.kauppa.aloita_asiointi()
         self.kauppa.lisaa_koriin(1)
         self.kauppa.lisaa_koriin(1)
         self.kauppa.tilimaksu("liisa", "11111")
@@ -77,9 +72,7 @@ class TestKauppa(unittest.TestCase):
         self.pankki_mock.tilisiirto.assert_called_with("liisa", 42, "11111", ANY, 10)
 
     def test_tilisiirtoa_kutsutaan_oikein_kun_toinen_ostettavista_tuotteista_loppu(self):
-        self.viitegeneraattori_mock.uusi.return_value = 42
 
-        self.kauppa.aloita_asiointi()
         self.kauppa.lisaa_koriin(1)
         self.kauppa.lisaa_koriin(3)
         self.kauppa.tilimaksu("liisa", "11111")
