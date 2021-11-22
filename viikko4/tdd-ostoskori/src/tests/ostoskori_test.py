28e4ebfb01f98b8652f25d1105bef86b94a072fe
susanna <susannae.repo@gmail.com>
2021-11-22 16:07:00 +0200

    lisää testit tuotteen lisäämiselle, toteuta tuotteen lisäys koriin ostoksena ja laske korin tavaroiden määrä

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -9,3 +9,9 @@ class TestOstoskori(unittest.TestCase):
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
+        mehu = Tuote("mehu", 3)
+        self.kori.lisaa_tuote(mehu)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 1)
